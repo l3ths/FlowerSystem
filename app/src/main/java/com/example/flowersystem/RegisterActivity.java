aa494036967850e1b769fe09df01d3b57f037244
l3ths <97070836+l3ths@users.noreply.github.com>
2023-07-04 18:20:20 +0700

    Register activity
    edit

--- app/src/main/java/com/example/flowersystem/RegisterActivity.java
+++ app/src/main/java/com/example/flowersystem/RegisterActivity.java
@@ -3,12 +3,30 @@ package com.example.flowersystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class RegisterActivity extends AppCompatActivity {
+    ImageView ivBack;
+    EditText etName;
+    EditText etEmail;
+    EditText etPhone;
+    EditText etPassword;
+    EditText etConfirmPassword;
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ivBack = findViewById(R.id.ivBack);
+        etName = findViewById(R.id.etName);
+        etEmail = findViewById(R.id.etEmail);
+        etPhone = findViewById(R.id.etPhone);
+        etPassword = findViewById(R.id.etPassword);
+        etConfirmPassword = findViewById(R.id.etConfirmPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+
     }
 }
