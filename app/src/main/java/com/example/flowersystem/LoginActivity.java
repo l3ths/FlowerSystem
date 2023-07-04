e6ff7ca24d77c0d8d25e46f7c6da4bc08de4e5a6
l3ths <97070836+l3ths@users.noreply.github.com>
2023-07-04 17:36:45 +0700

    login activity
    update login Activity

--- app/src/main/java/com/example/flowersystem/LoginActivity.java
+++ app/src/main/java/com/example/flowersystem/LoginActivity.java
@@ -3,12 +3,28 @@ package com.example.flowersystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    ImageView ivBack;
+    EditText etEmail;
+    EditText etPassword;
+    TextView tvForgotPassword;
+    TextView tvRegister;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ivBack = findViewById(R.id.ivBack);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        tvForgotPassword = findViewById(R.id.tvForgotPasswordRedirect);
+        tvRegister = findViewById(R.id.tvRegisterRedirect);
+
     }
 }
