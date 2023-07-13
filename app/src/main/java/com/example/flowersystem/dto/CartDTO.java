8c5db4d5f812daea53fa9b7823d7abd0da7f67eb
mai xuan quan <maixuanquan.mxq@gmail.com>
2023-07-13 21:58:50 +0700

    add cartDTO, cartApi

---
+++ app/src/main/java/com/example/flowersystem/dto/CartDTO.java
@@ -0,0 +1,37 @@
+package com.example.flowersystem.dto;
+
+
+import java.io.Serializable;
+
+public class CartDTO implements Serializable {
+    private Long id;
+    private FlowerDTO fLowerDTO;
+    private int quantity;
+
+    public CartDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public FlowerDTO getfLowerDTO() {
+        return fLowerDTO;
+    }
+
+    public void setfLowerDTO(FlowerDTO fLowerDTO) {
+        this.fLowerDTO = fLowerDTO;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
