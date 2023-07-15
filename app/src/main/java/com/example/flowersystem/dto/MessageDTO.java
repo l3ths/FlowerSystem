532f6835301c318e69c19fc7e63e8f1a693ae9fc
mai xuan quan <maixuanquan.mxq@gmail.com>
2023-07-15 22:18:00 +0700

    add messageDTO

---
+++ app/src/main/java/com/example/flowersystem/dto/MessageDTO.java
@@ -0,0 +1,17 @@
+package com.example.flowersystem.dto;
+
+
+public class MessageDTO {
+    private String msg;
+
+    public MessageDTO() {
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
