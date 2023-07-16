49f23419498b7f87b226bbc9d6ef0b110fdd9b4a
phaiduykota <phaiduykota@gmail.com>
2023-07-16 16:32:14 +0700

    add Intent

--- app/src/main/java/com/example/flowersystem/NotificationActivity.java
+++ app/src/main/java/com/example/flowersystem/NotificationActivity.java
@@ -2,13 +2,43 @@ package com.example.flowersystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class NotificationActivity extends AppCompatActivity {
-
+    ImageView ivBack;
+    ImageView ivOrders;
+    ImageView ivHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+        ivBack = findViewById(R.id.ivBack2);
+        ivOrders = findViewById(R.id.ivOrders);
+        ivHome = findViewById(R.id.ivHome);
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        ivOrders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NotificationActivity.this, OrdersActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ivHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NotificationActivity.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
