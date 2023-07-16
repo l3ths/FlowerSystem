6e8d8fab2930198d841be5d523fcc99b03876b9b
l3ths <97070836+l3ths@users.noreply.github.com>
2023-07-16 14:46:59 +0700

    order

--- app/src/main/java/com/example/flowersystem/OrdersActivity.java
+++ app/src/main/java/com/example/flowersystem/OrdersActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.flowersystem.api.CartApi;
@@ -29,16 +34,57 @@ public class OrdersActivity extends AppCompatActivity {
     RecyclerView rvOrder;
     OrderAdapter adapter;
     CustomerDTO CUSTOMER = Constants.LOGGED_IN_CUSTOMER;
+    TextView tvShipping;
+    TextView tvShipped;
+    TextView tvCanceled;
+    String STATUS = Constants.OrderStatusNumber.CREATED;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders);
         rvOrder = findViewById(R.id.rvOrder);
+        tvShipping = findViewById(R.id.tvShipping);
+        tvShipped = findViewById(R.id.tvShipped);
+        tvCanceled = findViewById(R.id.tvCanceled);
+
+        tvShipping.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvShipping.setBackgroundColor(getResources().getColor(R.color.ligt_orange));
+                tvShipped.setBackgroundColor(Color.WHITE);
+                tvCanceled.setBackgroundColor(Color.WHITE);
+                STATUS = Constants.OrderStatusNumber.CREATED;
+                onResume();
+            }
+        });
+        tvShipped.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvShipped.setBackgroundColor(getResources().getColor(R.color.ligt_orange));
+                tvShipping.setBackgroundColor(Color.WHITE);
+                tvCanceled.setBackgroundColor(Color.WHITE);
+                STATUS = Constants.OrderStatusNumber.SHIPPED;
+                onResume();
+            }
+        });
+        tvCanceled.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvCanceled.setBackgroundColor(getResources().getColor(R.color.ligt_orange));
+                tvShipped.setBackgroundColor(Color.WHITE);
+                tvShipping.setBackgroundColor(Color.WHITE);
+                STATUS = Constants.OrderStatusNumber.CANCELLED;
+                onResume();
+            }
+        });
+
         rvOrder.setLayoutManager(new LinearLayoutManager(OrdersActivity.this));
         adapter = new OrderAdapter(OrdersActivity.this);
         rvOrder.setAdapter(adapter);
     }
+
     protected void onResume() {
         super.onResume();
         retrieveTask();
@@ -55,11 +101,13 @@ public class OrdersActivity extends AppCompatActivity {
                     List<OrderDTO> list = response.body();
                     ArrayList<OrderFlower> orderFlowerList = new ArrayList<>();
                     for (int i = 0; i < list.size(); i++) {
-                        orderFlowerList.add(new OrderFlower(list.get(i)));
+                        if (TextUtils.equals(list.get(i).getOrderStatus(), STATUS)) {
+                            orderFlowerList.add(new OrderFlower(list.get(i)));
+                        }
                     }
                     adapter.setTasks(orderFlowerList);
-                    if (adapter.getItemCount()==0){
-                        Toast.makeText(OrdersActivity.this, "Giỏ hàng trống!", Toast.LENGTH_SHORT).show();
+                    if (adapter.getItemCount() == 0) {
+                        Toast.makeText(OrdersActivity.this, "Đơn hàng trống!", Toast.LENGTH_SHORT).show();
                     }
                 }
 
